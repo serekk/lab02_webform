fcc66db6577157d2304bed730fd5fae93b9f3efb
Karol <karolpoczta@gmail.com>
2020-04-23 15:37:35 +0200

    zmiany

--- lab02_webform/Default.aspx.cs
+++ lab02_webform/Default.aspx.cs
@@ -11,34 +11,67 @@ namespace lab02_webform
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                var obj = Session["Kalkulator"];
+
+                if (obj != null)
+                {
+                    var kalkulator = (Kalkulator)obj;
+                    TextBoxWartosc1.Text = kalkulator.a.ToString();
+                    TextBoxWartosc2.Text = kalkulator.b.ToString();
+                    DropDownList1.SelectedValue = kalkulator.op;
+                }
+
+                else
+                {
+                    if (Request.Cookies["Kalkulator"] != null)
+                    {
+                        TextBoxWartosc1.Text = Request.Cookies["Kalkulator"].Values["a"];
+                        TextBoxWartosc2.Text = Request.Cookies["Kalkulator"].Values["b"];
+                        DropDownList1.SelectedValue = Request.Cookies["Kalkulator"].Values["op"];
+                    }
+                }
+
+            }
+
         }
         protected void Button1_Click(object sender, EventArgs e) 
         {
+            var kalkulator = new Kalkulator();
 
-            int a = int.Parse(TextBoxWartosc1.Text);
-            int b = int.Parse(TextBoxWartosc2.Text);
 
-            var op = DropDownList1.SelectedValue;
+            kalkulator.a = int.Parse(TextBoxWartosc1.Text);
+            kalkulator.b = int.Parse(TextBoxWartosc2.Text);
+            kalkulator.op = DropDownList1.SelectedValue;
 
-            switch (op)
+            Session["Kalkulator"] = kalkulator;
+
+            Response.Cookies["Kalkulator"].Values["a"] = kalkulator.a.ToString();
+            Response.Cookies["Kalkulator"].Values["b"] = kalkulator.b.ToString();
+            Response.Cookies["Kalkulator"].Values["op"] = kalkulator.op;
+
+            switch (kalkulator.op)
             {
                 case "+":
-                    TextBoxWynik.Text = (a + b).ToString();
+                    TextBoxWynik.Text = (kalkulator.a + kalkulator.b).ToString();
                     break;
                 case "-":
-                    TextBoxWynik.Text = (a - b).ToString();
+                    TextBoxWynik.Text = (kalkulator.a - kalkulator.b).ToString();
                     break;
                 case "*":
-                    TextBoxWynik.Text = (a * b).ToString();
+                    TextBoxWynik.Text = (kalkulator.a * kalkulator.b).ToString();
                     break;
                 case "/":
-                    TextBoxWynik.Text = (a / b).ToString();
+                    TextBoxWynik.Text = (kalkulator.a / kalkulator.b).ToString();
                     break;
                 
                 default:
                     TextBoxWynik.Text = "Nieobslugiwany operator";
                     break;
             }
+
         }
+
     }
 }
---
+++ lab02_webform/Kalkulator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab02_webform
+{
+    public class Kalkulator
+    {
+        public int a;
+        public int b;
+        public string op;
+
+        public Kalkulator()
+        {
+        }
+
+        public Kalkulator(int a, int b, string op)
+        {
+            this.a = a;
+            this.b = b;
+            this.op = op ?? throw new ArgumentNullException(nameof(op));
+        }
+    }
+}
